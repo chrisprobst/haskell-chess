c351e680e112a168a0ef4f0ee04d2006c398bef1
Christopher Probst <christopher.probst@hhu.de>
2013-10-04 11:21:46 +0200

    Implement rook & bishop movement

--- chess.hs
+++ chess.hs
@@ -3,11 +3,12 @@ import Data.Maybe
 import Control.Monad
 
 --------------------------------------------------------
--------- Useful array operations -----------------------
 --------------------------------------------------------
 
+-- Double composition
+(.:) = (.) . (.)
 
--- 2d Array element copy
+
 move :: (Int, Int) -> (Int, Int) -> [[Maybe a]] -> [[Maybe a]]
 move (x, y) (x2, y2) board = fullMat
   where
@@ -18,7 +19,6 @@ move (x, y) (x2, y2) board = fullMat
     emptyRow = replace x Nothing (board !! y)
 
 
--- Replace an element in an 1d array
 replace :: Int -> a -> [a] -> [a]
 replace i x xs = left ++ (x : (tail right))
   where
@@ -45,6 +45,17 @@ removeNothing = map fromJust . filter isJust
 
 --------------------------------------------------------
 --------------------------------------------------------
+
+data Direction = N
+               | S
+               | E
+               | W
+               | NE
+               | NW
+               | SE
+               | SW
+               deriving (Show, Eq)
+
 
 data Piece = Pawn
            | Rook
@@ -94,40 +105,26 @@ showMColoredPiece (Just x) = showColoredPiece x
 
 
 createMColoredPieces :: Color -> [Piece] -> [MColoredPiece]
-createMColoredPieces color = map (Just . Create color)
+createMColoredPieces = map . (Just .: Create)
 
 
-getColor :: (Int, Int) -> Board -> Maybe Color
-getColor coord board = fmap (\(Create color _) -> color) (get coord board)
-
-
-isWhite :: (Int, Int) -> Board -> Bool
-isWhite coord board = case getColor coord board of
-  Just White -> True
-  _ -> False
-
-
-isBlack :: (Int, Int) -> Board -> Bool
-isBlack coord board = case getColor coord board of
-  Just Black -> True
-  _ -> False
-
-
-moves :: (Int, Int) -> Board -> Maybe [((Int, Int), MColoredPiece)]
-moves coord board = case getPiece coord board of
-  Just (Create color piece) -> case piece of
-    Pawn -> Just (pawnMoves coord color board)
-    Rook -> Just (rookMoves coord color board)
-    _ -> Nothing
-  _ -> Nothing
-
-
-checkMove :: ((Int, Int), MColoredPiece) -> Maybe ((Int, Int), MColoredPiece)
-checkMove (coords, mp) = fmap (const (coords, mp)) $ checkCoords coords
+extractColor :: MColoredPiece -> Maybe Color
+extractColor = fmap (\(Create color _) -> color)
 
 
 validMoves :: (Int, Int) -> Board -> Maybe [((Int, Int), MColoredPiece)]
 validMoves = fmap (removeNothing . map checkMove) .: moves
+  where
+    -- Check the bounds
+    checkMove (coords, mp) = fmap (const (coords, mp)) $ checkCoords coords
+    -- Map the piece type to the specific function
+    moves coord board = case getPiece coord board of
+      Just (Create color piece) -> case piece of
+        Pawn -> Just (pawnMoves coord color board)
+        Rook -> Just (rookMoves coord color board)
+        Bishop -> Just (bishopMoves coord color board)
+        _ -> Nothing
+      _ -> Nothing
 
 ----------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------
@@ -135,17 +132,62 @@ validMoves = fmap (removeNothing . map checkMove) .: moves
 ----------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------
 
-(.:) = (.) . (.)
+
+collectPieces :: Board -> Color -> Direction -> Int -> (Int, Int) -> [((Int, Int), MColoredPiece)]
+collectPieces board ownColor dir n (c, r) = map (\p -> (p, getPiece p board)) . map halfTuple $
+                                            takeWhile verifyPosition dir2HistoricCoords
+  where
+    halfTuple (_,_,x,y) = (x, y)
+
+    -- Create a list of historic positions
+    dir2HistoricCoords = map unpack $ zip stream (tail stream)
+      where
+        unpack ((a, b), (c, d)) = (a, b, c, d)
+        stream = dir2Coords dir (c, r) n
+        dir2Coords N (c, r) n = [(c, r - i) | i <- [0..n]]
+        dir2Coords S (c, r) n = [(c, r + i) | i <- [0..n]]
+        dir2Coords W (c, r) n = [(c - i, r) | i <- [0..n]]
+        dir2Coords E (c, r) n = [(c + i, r) | i <- [0..n]]
+        dir2Coords NE (c, r) n = [(c + i, r - i) | i <- [0..n]]
+        dir2Coords NW (c, r) n = [(c - i, r - i) | i <- [0..n]]
+        dir2Coords SE (c, r) n = [(c + i, r + i) | i <- [0..n]]
+        dir2Coords SW (c, r) n = [(c - i, r + i) | i <- [0..n]]
+
+    -- Verifies the position
+    verifyPosition  (oc, or, c, r) =
+      noPiece || (color /= ownColor && (noOldPiece || oldColor == ownColor))
+      where
+        coords = (c, r)
+        oldCoords = (oc, or)
+
+        piece = getPiece coords board
+        noPiece = isNothing piece
+
+        oldPiece = getPiece oldCoords board
+        noOldPiece = isNothing oldPiece
+
+        color = fromJust $ extractColor piece
+        oldColor = fromJust $ extractColor oldPiece
 
 
 rookMoves :: (Int, Int) -> Color -> Board -> [((Int, Int), MColoredPiece)]
-rookMoves (c, r) color board = up ++ down ++ left ++ right
+rookMoves coords color board = up ++ down ++ left ++ right
   where
-    up = map (\coords -> (coords, getPiece coords board)) [(c, r - i) | i <- [1..8], isJust (getPiece (c, r - (i-1)) board)]
-    down = map (\coords -> (coords, getPiece coords board)) [(c, r + i) | i <- [1..8], isJust (getPiece (c, r + (i-1)) board)]
-    left = map (\coords -> (coords, getPiece coords board)) [(c - i, r) | i <- [1..8], isJust (getPiece (c - (i-1), r) board)]
-    right = map (\coords -> (coords, getPiece coords board)) [(c + i, r) | i <- [1..8], isJust (getPiece (c + (i-1), r) board)]
+    positions dir = collectPieces board color dir 8 coords
+    up = positions N
+    down = positions S
+    left = positions W
+    right = positions E
 
+
+bishopMoves :: (Int, Int) -> Color -> Board -> [((Int, Int), MColoredPiece)]
+bishopMoves coords color board = ne ++ nw ++ se ++ sw
+  where
+    positions dir = collectPieces board color dir 8 coords
+    ne = positions NE
+    nw = positions NW
+    se = positions SE
+    sw = positions SW
 
 
 pawnMoves :: (Int, Int) -> Color -> Board -> [((Int, Int), MColoredPiece)]
